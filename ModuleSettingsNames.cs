17afcf520dce21eeb5f298d3302a7fe7449d04c2
Scott Gowell <scott@gravityworksdesign.com>
2013-07-01 14:22:24 -0400

    Initial Checkin

---
+++ ModuleSettingsNames.cs
@@ -0,0 +1,8 @@
+namespace GravityWorks.BecomeUser
+{
+    public static class ModuleSettingsNames
+    {
+        public static string BlockedRole = "BlockedRole";
+        public static string SessionObject = "SessionObject";
+    }
+}
